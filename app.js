a7e67fadfc25dc30bf4e0eaf4c4b4ac225d90e2f
keyserfaty <keserfaty@gmail.com>
2016-09-16 01:44:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,128 @@
+const test1 = {
+  "title": "Desplazan al director del penal de Ezeiza por un presunto plan de fuga de \"Mi Sangre\"",
+  "subtitle": "18:57 › LO VINCULARON JUNTO CON OTROS DOS FUNCIONARIOS DE ESA CARCEL",
+  "text": "El Servicio Penitenciario Federal pasó a disponibilidad al director del penal de Ezeiza, Héctor Retamozo, y otros dos funcionarios luego de que se descubriera un presunto plan de fuga por parte de un grupo comando del supuesto narco colombiano Jésus López Londoño, alias \"Mi Sangre\".Según consignó la agencia Télam, López Londoño, el ciudadano colombiano detenido desde 2012 por estar acusado de ser parte de una organización narcotraficante, iba a ser rescatado del penal de Ezeiza por un grupo comando para evitar su extradición a Estados Unidos.Fuentes del Servicio Penitenciario Federal (SPF) informaron a la agencia oficial que la situación se descubrió ayer a las 14, luego de que se detectara que María Gabriela Ricagno, abogada de López Londoño, intentara ingresar a la cárcel con un plano de los terrenos del penal de Ezeiza.En el mapa -siempre según Télam- también figuraban la ubicación detallada de comisarí­as y otras dependencias policiales próximas al penal, así como los barrios aledaños hasta el partido de Esteban Echeverría y una posible línea de escape trazada en rojo hacia Cañuelas.Ante esta situación, la abogada fue demorada y recién liberada a las 21 y se dio intervencion al juez federal de Lomas de Zamora Federico Villena y a todas las fuerzas de seguridad para que estuvieran alerta.El magistrado ordenó que se revisara la celda de López Londoño y allí se habrían hallado fotocopias de expedientes y, dentro de ellas, en un hueco, un teléfono celular Iphone, dos chips y un módem portátil.El presunto narco también guardaba un centenar de discos compactos, que al ser revisados tenían grabados videos del espacio aéreo del penal, lo que -sumado a los elementos anteriores- hizo creer a los investigadores que iba a ser rescatado por un grupo comando, por ejemplo, utilizando un helicóptero.Por eso, esta tarde fue pasado a disponibilidad el director del penal, Héctor Retamozo, y otros dos funcionarios que se cree que tuvieron vinculación con el ingreso del teléfono y los otros elementos hallados.En tanto, la Jefatura de Asuntos Internos del SPF investiga a todos los efectivos que mantuvieron contacto con el detenido el viernes y los días anteriores, que por ahora no están cumpliendo funciones.López Londoño tení­a pedido de captura internacional, acusado de ser jefe del cartel \"Los Urabeños\", y estaba negociando desde hace algunos meses su entrega ante la Justicia de su paí­s. Fue apresado en la ciudad de Pilar el 30 de octubre de 2012 y el operativo estuvo a cargo de la entonces Secretarí­a de Inteligencia (SI), que contó con la colaboración de la Dirección de Policí­a Judicial Colombiana y de Interpol Colombia.Pese a que Estados Unidos pidió formalmente su extradición apenas dos meses después de la detención, la defensa de \"Mi Sangre\" presentó numerosos recursos ante la Justicia para recusar a distintos jueces que iban interviniendo en su causa.",
+  "paper": "pagina12"
+};
+
+const test2 = {
+  "title": "Descubren un plan de fuga de \"Mi Sangre\" y separan preventivamente al director del penal de Ezeiza",
+  "subtitle": "El líder narco colombiano tenía un celular y estaba por obtener un mapa con la ubicación detallada de comisarías y dependencias policiales próximas a la cárcel",
+  "text": "El director del penal de Ezeiza, Héctor Retamozo, fue separado de su cargo preventivamente luego de que se descubriera que el líder narco colombiano Henry de Jesús López Londoño, alias         \"Mi Sangre\"  planeaba fugarse.Así lo informaron fuentes oficiales a LA NACION. No descartan que se desvincule a otros funcionarios en el  marco de la misma investigación.El Complejo Penitenciario Federal I, conocido como la cárcel de Ezeiza, es el penal más grande del país. Habilitado en 1999, tiene capacidad para 2193 hombres y, según los últimos datos disponibles, hay 2069 presos alojados en esta prisión. La mayoría de los internos están detenidos por infracción a la ley de drogas, delitos contra la propiedad y contra las personas.El penal cuenta con seis módulos, una unidad de ingreso y el Hospital Penitenciario Central (HPC). Cada módulo se divide en pabellones, que cuentan con celdas individuales y un sector común.En los últimos años, se convirtió en el lugar de alojamiento de los presos de los casos más resonantes.  Además de Londoño, se encuentran en Ezeiza       José López,  ex secretario de Obras Públicas,  Jorge Chueco, el abogado cercano a       Lázaro Báez, mencionado en la declaración de Leonardo Fariña;       Martín y Cristian Lanatta y Víctor Schillaci,  condenados por el triple crimen de General Rodríguez; Jorge Mangeri, el encargado del edificio en el que vivía Ángeles Rawson;       Ricardo Jaime,  ex funcionario kirchnerista, Lázaro Báez, el empresario afín a los Kirchner; y Daniel Pérez Gadin, el contador de Lázaro Báez.\"Mi sangre\" está detenido en la Argentina desde 2012. Esta semana se descubrió que tenía en su poder un teléfono celular y que estaba por obtener un mapa con la ubicación detallada de comisarías y dependencias policiales próximas al penal, por lo que autoridades judiciales y penitenciarias creen que planeaba fugarse, informó el Servicio Penitenciario Federal (SPF).Los objetos fueron detectados ayer por personal del SPF, quienes por su estatus de \"interno de alto riesgo\" someten a \"Mi Sangre\" a controles de seguridad exhaustivos especiales.Según dijeron los voceros consultados, el mapa le fue secuestrado a su abogada defensora, María Gabriela Ricagno, entre distintos papeles que presentó ante la requisa en el momento en el que ingresaba este viernes a visitar a su cliente, en el pabellón de máxima seguridad del penal de Ezeiza.El teléfono, en tanto, fue incautado durante una revisión en la celda de López Londoño.A partir de ambos elementos, las autoridades sospechan que \"Mi Sangre\" estaba organizando fugarse de la cárcel y apartaron a los efectivos penitenciarios encargados de su custodia hasta que se lleve adelante una investigación interna que determine si prestaron colaboración para hacerse con el celular.De acuerdo con fuentes penitenciarias, las sospechas acerca un intento de evasión del sindicado narco colombiano se acrecientan por su situación procesal, ya que López Londoño apeló el fallo del juez federal 2 Sebastián Ramos, que en mayo resolvió su extradición a Estados Unidos, y actualmente aguarda la decisión de la Corte Suprema de Justicia de la Nación por su caso.López Londoño fue apresado el 30 de octubre de 2012 cuando cenaba junto a allegados y custodios en el restaurante \"Fettucine Mario\" del partido bonaerense de Pilar.El operativo de detención estuvo a cargo de la entonces Secretaría de Inteligencia (SI), que contó con la colaboración de la Dirección de Policía Judicial Colombiana y de Interpol Colombia.López Londoño, que había ingresado diez meses antes a la Argentina, tenía pedido de captura internacional, acusado de ser jefe del cartel \"Los Urabeños\", y estaba negociando desde hace algunos meses su entrega ante la Justicia de su país.Pese a que Estados Unidos pidió formalmente su extradición apenas dos meses después de la detención, la defensa de \"Mi Sangre\" presentó numerosos recursos ante la Justicia para recusar a distintos jueces que iban interviniendo en su causa.Además, logró suspender en dos ocasiones el juicio que debía efectuarse antes de ser enviado al tribunal de Florida, ya que su intención es quedarse en el país, donde viven su esposa y sus hijos.Con la colaboración de Sol Amaya",
+  "paper": "lanacion"
+};
+
+const upperList = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'];
+const existy = arg => arg != null;
+const isUpper = elem => upperList.indexOf(elem[0]) !== -1;
+
+const parsedWords = news =>
+  Object.keys(news)
+    .map(e => news[e])
+    .join()
+    .split(new RegExp('\"|,|-|›| '))
+    .map(e => e.trim())
+    .map(e => e.includes('.') ? e[1].toLocaleLowerCase() + e.slice(2) : e)
+    .filter(e => e.length);
+
+const getNames = news =>
+  news
+    .reduce((res, e, i, arr) => {
+      if (isUpper(e) && existy(arr[i - 1]) && !isUpper(arr[i - 1])) {
+        res.push(e);
+        return res;
+      }
+
+      if (isUpper(e) && existy(arr[i - 1]) && isUpper(arr[i - 1])) {
+        res[res.length - 1] = res[res.length - 1] + ' ' + e;
+        return res;
+      }
+
+      return res;
+    }, []);
+
+const getNamesCount = names =>
+  names
+    .reduce((res, e) => {
+      const includesName = Object.keys(res).filter(key => key.includes(e) || key === e);
+
+      if ((res.hasOwnProperty(e) || includesName) && includesName.length > 0) {
+        res[e] = includesName.length + 1;
+      }
+
+      if (!res.hasOwnProperty(e)) {
+        res[e] = 1;
+      }
+
+      return res;
+    }, {});
+
+const buildArray = obj =>
+  Object.keys(obj)
+    .reduce((res, key) => {
+      const e = {
+        name: key,
+        count: obj[key]
+      };
+
+      res.push(e);
+      return res;
+    }, []);
+
+const getRelevantNames = names =>
+  buildArray(names)
+    .filter(elem => elem.count !== 1)
+    .sort((a, b) => a.count < b.count)
+    .reduce((res, elem) => {
+      res[elem.name] = elem.count;
+      return res;
+    }, {});
+
+const namesPagina = getNames(parsedWords(test1));
+const relevantNamesCountPagina = getRelevantNames(getNamesCount(namesPagina));
+
+const namesLN = getNames(parsedWords(test2));
+const relevantNamesCountLN = getRelevantNames(getNamesCount(namesLN));
+
+const appearancePercentage = names =>
+  buildArray(names)
+    .reduce((res, elem) => {
+      const total = buildArray(names).reduce((sum, e) => sum + e['count'], 0);
+
+      res[elem.name] = {
+        percentage: Number((elem.count / total * 100).toFixed(2))
+      };
+      
+      return res;
+    }, {});
+
+console.log(appearancePercentage(relevantNamesCountPagina));
+console.log(appearancePercentage(relevantNamesCountLN));
+
+// need to filter repeated words like mi -> mi sangre, lodoño -> lopez lodoño
